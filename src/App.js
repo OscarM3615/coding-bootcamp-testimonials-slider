84ff64b7b349aa0564a42f7ed899c04da254bf07
Óscar Miranda <oscarmiranda3615@gmail.com>
2020-08-08 19:45:24 -0600

    Finished design

--- src/App.js
+++ src/App.js
@@ -9,13 +9,13 @@ function App() {
 			name: 'Tanya Sinclair',
 			position: 'UX Engineer',
 			image: '/images/image-tanya.jpg',
-			cite: '“ I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future. ”'
+			cite: 'I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future.'
 		},
 		{
 			name: 'John Tarkpor',
 			position: 'Junior Front-end Developer',
 			image: '/images/image-john.jpg',
-			cite: '“ If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ”'
+			cite: 'If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer.'
 		}
 	]
 
--- src/components/Slider.js
+++ src/components/Slider.js
@@ -6,14 +6,59 @@ class Slider extends Component {
 		super(props)
 
 		this.state = {
-			testimonials: props.data
+			testimonials: props.data,
+			currentIndex: 0
 		}
+
+		this.goPrev = this.goPrev.bind(this)
+		this.goNext = this.goNext.bind(this)
+	}
+
+	goPrev() {
+		let newIndex = this.state.currentIndex - 1
+		if (newIndex < 0) newIndex = this.state.testimonials.length - 1
+
+		this.setState((prevState) => ({
+			testimonials: prevState.testimonials,
+			currentIndex: newIndex
+		}))
+	}
+
+	goNext() {
+		let newIndex = this.state.currentIndex + 1
+		if (newIndex === this.state.testimonials.length) newIndex = 0
+
+		this.setState((prevState) => ({
+			testimonials: prevState.testimonials,
+			currentIndex: newIndex
+		}))
 	}
 
 	render() {
+		let name = this.state.testimonials[this.state.currentIndex].name
+		let position = this.state.testimonials[this.state.currentIndex].position
+		let image = this.state.testimonials[this.state.currentIndex].image
+		let cite = this.state.testimonials[this.state.currentIndex].cite
+
 		return (
 			<div className="slider">
-				{this.state.testimonials[0].name}
+				<div className="slider__picture">
+					<img src={image} alt={`"${name}"`} />
+
+					<div className="buttons">
+						<button id="buttonPrev" className="button" onClick={this.goPrev}>&nbsp;</button>
+						<button id="buttonNext" className="button" onClick={this.goNext}>&nbsp;</button>
+					</div>
+				</div>
+				
+				<div className="slider__text">
+					<p className="cite__text">“ {cite} ”</p>
+
+					<div className="author__info">
+						<p className="cite__author">{name}</p>
+						<p className="author__position">{position}</p>
+					</div>
+				</div>
 			</div>
 		)
 	}
